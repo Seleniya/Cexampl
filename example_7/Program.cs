2a957ca943403bdbe33f61860a5dee83e7a560d6
Кристина Врещ <cikoriya@gmail.com>
2023-05-24 08:34:55 +0300

    коммит _6 и _7

---
+++ example_7/Program.cs
@@ -0,0 +1,15 @@
+﻿int A = 1;
+int B = 3;
+int C = 8;
+int D = 9;
+
+
+int max = A;
+
+if (A > max) max = A;
+if (B > max) max = B;
+if (C > max) max = C;
+if (D > max) max = D;
+
+Console.Write("max = ");
+Console.WriteLine(max);
---
+++ examples_6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Как вас зовут?");
+string username = Console.ReadLine()!;
+
+if(username == "Маша")
+{
+    Console.WriteLine("Супер! Это Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
